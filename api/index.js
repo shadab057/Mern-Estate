167866ac116967606200747b36af990cc5246fc2
shadab057 <mohdshadab4629@gmail.com>
2024-05-16 00:23:30 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import { log } from 'console';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(()=>{
+console.log('Connected to MongoDB!');
+}).catch((err)=>{
+    console.log(err);
+})
+
 
 const app= express();
 
 app.listen(3000, ()=>{
-    console.log('server is runnig on port 3000!!!')
+    console.log('server is runnig on port 3000!!!!!!')
 })
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -27,6 +27,7 @@ export default function Header() {
     <Link to='/sign-in'>
     <li className=" text-slate-700 hover:underline ">Sign in</li>
     </Link>
+            
         </ul>
         </div>
     </header>
