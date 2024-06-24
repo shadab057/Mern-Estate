4b8a14a1f6d957444c37828c0515ae92ce99ce96
shadab057 <mohdshadab4629@gmail.com>
2024-06-24 17:40:43 +0530

    complete show user listings functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,7 +1,7 @@
 import bcryptjs from 'bcryptjs'
 import User from '../models/User.model.js'
 import { errorHandler } from '../utils/error.js';
-import Listing from '../models/listing.model.js';
+import Listing from '../models/listingmodel.js';
 
 export const test = (req, res)=>{
     res.json({
