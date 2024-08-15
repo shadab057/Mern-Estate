a05a829d370757ca88fbd05c907319f5ed2635b1
shadab057 <mohdshadab4629@gmail.com>
2024-08-15 18:54:47 +0530

    Create serch API route

--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -9,6 +9,7 @@ import { app } from '../firebase';
 import { useSelector } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom';
 
+
 export default function CreateListing() {
   const { currentUser } = useSelector((state) => state.user);
   const navigate = useNavigate();
